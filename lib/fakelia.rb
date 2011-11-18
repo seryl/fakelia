8bfb50a21ae40116808cf7e3f7ab4b77a5d41330
Josh Toft <jtoft@zinio.com>
2011-11-18 14:23:28 -0800

    Fakelia now adds all of the proper children

---
+++ lib/fakelia.rb
@@ -0,0 +1,20 @@
+$:.unshift File.dirname __FILE__
+
+require 'gmetric'
+require 'active_support/core_ext'
+require 'eventmachine'
+require 'json'
+
+# Long running ganglia client/forwarder for windows and OSX.
+module Fakelia
+  # Gem Version
+  VERSION = '1.0.0'
+  
+  # Empty module to fill with script modules
+  module Scripts
+  end
+end
+
+require 'fakelia/util'
+require 'fakelia/client'
+require 'fakelia/application'
